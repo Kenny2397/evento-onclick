ed832ee6e77fd7eb685605472c2409cdea98e45f
Kenny Luque <kenny.luque.t@uni.pe>
2022-02-20 19:46:37 -0500

    evento-onclick

---
+++ script.js
@@ -0,0 +1,10 @@
+function button(element) {
+    element.innerText = "Logout";
+
+}
+function likes() {
+    alert("ninja was liked!");
+}
+function delete_button(element) {
+    element.remove();
+}
